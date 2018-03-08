4ab75b316b69dbc22227a01b79e071bb9b0eeee0
Halocline <mdglissmann@gmail.com>
2018-03-08 09:56:32 -0700

    Added express-handlebars middleware
    Added express-handlebars middleware

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//
+// server.js
 //
 //
 //
@@ -8,6 +8,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const exphbs = require('express-handlebars')
 const publication = require('../entities/publication/publication')
 
 publication.helloWorld()
@@ -24,8 +25,34 @@ const start = (options) => {
 
     // Initialize express app and middlewares
     const app = express()
-    app.use( morgan('dev') )
-    app.use( helmet() )
+    app.use( morgan('dev') )    // Logging
+    app.use( helmet() )         //
+    app.use( express.static(__dirname + '../public') )
+
+    // Setting express-handlebars as view engine
+    app.engine('.hbs', () => {
+      reject( new Error('Could not set express-handlebars as express app engine.') )
+      exphbs(
+        {
+          defaultLayout: 'main',
+          extname: '.hbs',
+          layoutsDir: path.join(__dirname, '../views/layouts')
+        }
+      )
+    })
+    app.set('view engine', () => {
+      reject( new Error('Could not set express handlebars as express app view engine.') )
+      '.hbs'
+    })
+    app.set('views', () => {
+      reject( new Error('Could not set default views directory.') )
+      path.join(__dirname, '../views')
+    })
+
+    app.use( (req, res, next) => {
+    	console.log(req.headers)
+    	next()
+    })
     app.use( (err, req, res, next) => {
       reject( new Error('Something went wrong!, err:' + err) )
       res.status(500).send('Something went wrong!')
